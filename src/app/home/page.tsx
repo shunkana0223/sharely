9a9a01f5af773ad9e7ccc5887ec97db1e7fb23d2
Shun <shun.tsuruoka06@gmail.com>
2024-09-16 23:28:28 +0900

    first commit

---
+++ src/app/home/page.tsx
@@ -0,0 +1,30 @@
+
+export default function Home()  {
+    return (
+        <>
+          <header>
+            <br />
+                Sharely
+            </header>
+
+            <div className="firsts-block">
+            </div>
+            <div className="seconds-block">
+                <h1>Sharelyとは</h1>
+                Sharelyは一言であらわすと検索機能付き情報共有ツールです。
+                <br/>
+                あなたは仕事やアルバイトでこんな経験ありませんか？
+                <br />
+                「○○ってどうすればいいんやっけ？　どっかでで見た(聞いた）気がするけどどこで見た(聞いた）のか忘れた！
+                <br />
+                やばいお客さんめっちゃ待たせてる！　どうすればいいんやー！！」
+                <br />
+                こんなお悩みはSharelyを使えばすべて解決します！
+            </div>
+            <div className="link-block">
+                <a href="">ログイン</a>
+                <a href="">新規登録</a>
+            </div>
+        </>
+    )
+}
